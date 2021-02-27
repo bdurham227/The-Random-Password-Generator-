932c7b04dc8030de5995464313b2b15ebd7397f4
bdurham227 <bdurham227@gmail.com>
2021-02-27 13:28:58 -0700

    updating JS- didnt like where I was going with the first script- felt problematic

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-// Define the variables
-var generateBtn = document.querySelector("#generate");
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -8,61 +7,36 @@ function writePassword() {
 };
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
 // creating the arrays for the variables
-var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-var special = [ "!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?","~" ];
-var lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var upper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var passwordArray = [];
 
-function generatePassword () {
+let passLength = 0;
+let typeNumber = "false";
+let typeUpper = "false";
+let typeLower = "false";
+let typeSpecial = "false";
+let newPassword = [];
+var generateBtn = document.querySelector("#generate");
 
-    var confirmLength = prompt("password must be 8 to 128");
+
+
+
+const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+const special = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '{', '}', ':', '"', '<', '>', '?', '/', '"'];
+const lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+const upper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+const passwordArray = [];
+
+
+
+
+
+
+
+
+
+
+
+
     
-    
-    
-    while 
-    (confirmLength <= 7 || confirmLength >= 129) {
-        alert("password length mustt be this long")
-        
-    }
-    if (confirmLength >= 8 && confirmLength <= 128) {
-    var confirmUpper = confirm("ya want uppers");
-    var confirmLower = confirm("ya want lowers");
-    var confirmNumbers = confirm("how bout some numbas");
-    var confirmSpecial = confirm("are you special?");
-    }
-    
-    if (!confirmUpper && !confirmLower && !confirmNumbers && !confirmSpecial) {
-        alert("please choose at least one for your password"); 
-        return confirmLength;
-    }
-    
-    
-    
-    
-    if (confirmUpper == true) {
-        passwordArray += passwordArray.concat(upperCase);
-    }
-    
-    if (confirmLower == true) {
-        passwordArray += passwordArray.concat(lowerCase);
-    }
-    
-    if (confirmNumbers == true) {
-    passwordArray += passwordArray.concat(numbers);
-    }
-    
-    if (confirmSpecial == true) {
-    passwordArray += passwordArray.concat(special);
-    }
-    
-    
-    var result = "";
-    
-    for (var i = 0; i < confirmLength; i++) {
-    result = result + passwordArray[Math.floor(Math.random() * passwordArray.length)];
-    }
-    return result;
-    
-}    
