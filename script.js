99d9bab4f0be50ddd7f5da79eda2e1532bdd1344
bdurham227 <bdurham227@gmail.com>
2021-02-27 13:42:44 -0700

    adding more to password generators Js

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ let typeLower = "false";
 let typeSpecial = "false";
 let newPassword = [];
 var generateBtn = document.querySelector("#generate");
+
 //write password to the #password input
 function writePassword() {
     var password = generatePassword();
@@ -15,17 +16,48 @@ function writePassword() {
     passwordText.value = password;
   };
 
-  // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
-
-
+ 
 
 const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 const special = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '{', '}', ':', '"', '<', '>', '?', '/', '"'];
-const lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-const upper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+const lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+const uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 const passwordArray = [];
+
+ function getInput() {
+     passLength = prompt("Please choose a length bettwe 8 and 128 characters for your password: ");
+     typeUpper = confirm("Would you like Uppercase letters in your password today?");
+     typeLower = confirm("Would you like Lowercase letters in your password today?");
+     typeNumber = confirm("Would you like numbers in your password today?");
+     typeSpecial = confirm("Would you like special characters in your password today?");
+ };
+
+ let optionsArray = [];
+
+ function generateOptions() {
+     if (typeUpper == true)
+     optionsArray = optionsArray.concat(uppercase);
+
+     if (typeLower == true)
+     optionsArray = optionsArray.concat(lowerrcase);
+
+     if (typeNumber == true)
+     optionsArray = optionsArray.concat(number);
+     
+     if (typeSpecial == true)
+     optionsArray = optionsArray.concat(special);
+
+
+
+
+
+
+
+
+
+
+
+ }
 
 
 
